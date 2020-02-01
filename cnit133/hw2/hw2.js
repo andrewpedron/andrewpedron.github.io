bf68c387f96193ebaf185b4e87192baedd1e2030
Andrew Pedron <andrew.pedron@flexport.com>
2020-02-01 12:00:49 -0800

    fade results

--- cnit133/hw2/hw2.js
+++ cnit133/hw2/hw2.js
@@ -91,7 +91,7 @@ function process2() {
 };
 
 $(document).ready(function(){
-  $("button").click(function(){
+  $("fade").click(function(){
     $("#div1").fadeTo("slow", 0.15);
     $("#div2").fadeTo("slow", 0.4);
     $("#div3").fadeTo("slow", 0.7);
