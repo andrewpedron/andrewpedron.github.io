3b81312761981cf3038a38755f8c8d75e312ddca
Andrew Pedron <andrew.pedron@flexport.com>
2020-02-16 17:30:34 -0800

    toFixed

--- cnit133/hw3/hw3.js
+++ cnit133/hw3/hw3.js
@@ -56,8 +56,8 @@ function process2() {
   item2_total = parseInt(number2)*129.75;
   item3_total = parseInt(number3)*99.95;
   item4_total = parseInt(number4)*350.89;
-  sum = item1_total + item2_total + item3_total + item4_total;
-  earnings = (sum*.09)+200;
+  sum = item1_total + item2_total + item3_total + item4_total.toFixed(2);
+  earnings = (sum*.09)+200.toFixed(2);
 
   document.getElementById("item1").innerHTML = number1;
   document.getElementById("total1").innerHTML = item1_total;
