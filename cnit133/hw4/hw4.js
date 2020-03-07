585f6fb99e3c5f8281043f4d900f4fa00361af08
Andrew Pedron <andrew.pedron@flexport.com>
2020-03-06 20:31:19 -0800

    tables

--- cnit133/hw4/hw4.js
+++ cnit133/hw4/hw4.js
@@ -19,9 +19,81 @@ function everyThird() {
   document.getElementById("everyThirdSum").innerHTML = sum;
 }
 
+function compoundFive() {
+  var tbody = document.createElement('tbody'), tr, td, row, cell, amount = 1000;
+  for (row = 1; row <= 10; row++) {
+      amount *= 1.05;
+      tr = document.createElement('tr');
+      for (cell = 0; cell <= 2; cell++) {
+          td = document.createElement('td');
+          if(cell == 0) {
+            td.innerHTML = row;
+          }
+          else if(cell == 1){
+            td.innerHTML = '5%';
+          }
+          else{
+            td.innerHTML = amount.toLocaleString("en", {style: "currency", currency: "USD"});
+          }
+          tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+  }
+  document.getElementById('compound').appendChild(tbody);
+}
+
+function compoundSix() {
+  var tbody = document.createElement('tbody'), tr, td, row, cell, amount = 1000;
+  for (row = 1; row <= 10; row++) {
+      amount *= 1.06;
+      tr = document.createElement('tr');
+      for (cell = 0; cell <= 2; cell++) {
+          td = document.createElement('td');
+          if(cell == 0) {
+            td.innerHTML = row;
+          }
+          else if(cell == 1){
+            td.innerHTML = '6%';
+          }
+          else{
+            td.innerHTML = amount.toLocaleString("en", {style: "currency", currency: "USD"});
+          }
+          tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+  }
+  document.getElementById('compound').appendChild(tbody);
+}
+
+function compoundSeven() {
+  var tbody = document.createElement('tbody'), tr, td, row, cell, amount = 1000;
+  for (row = 1; row <= 10; row++) {
+      amount *= 1.07;
+      tr = document.createElement('tr');
+      for (cell = 0; cell <= 2; cell++) {
+          td = document.createElement('td');
+          if(cell == 0) {
+            td.innerHTML = row;
+          }
+          else if(cell == 1){
+            td.innerHTML = '7%';
+          }
+          else{
+            td.innerHTML = amount.toLocaleString("en", {style: "currency", currency: "USD"});
+          }
+          tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+  }
+  document.getElementById('compound').appendChild(tbody);
+}
+
 function start() {
   everyFourth();
   everyThird();
+  compoundFive();
+  compoundSix();
+  compoundSeven();
 }
 
 
