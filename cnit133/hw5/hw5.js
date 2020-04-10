1ddb0b0c56f1ab1731a8332b5838ce8f847bcfb7
Andrew Pedron <andrew.pedron@flexport.com>
2020-04-09 18:29:42 -0700

    part1

--- cnit133/hw5/hw5.js
+++ cnit133/hw5/hw5.js
@@ -1,110 +1,30 @@
-function everyFourth() {
-  let number, prod = 1, sum = 0;
-  for (number = 5; number <= 21; number+=4) {
-    prod = number * prod;
-    sum = number + sum;
+function validate_checkboxes() {
+
+  if ($('input[type=checkbox]:checked').length == 0) {
+      // alert ( "ERROR! You need to select a checkbox" );
+      document.getElementById("box1").setAttribute('required');
+      document.getElementById("box2").setAttribute('required');
+    } else {
+      document.getElementById("box1").removeAttribute('required');
+      document.getElementById("box2").removeAttribute('required');
+    }
   }
-  document.getElementById("everyFourthProd").innerHTML = prod;
-  document.getElementById("everyFourthSum").innerHTML = sum;
-}
 
-function everyThird() {
-  let number = 3, prod = 1, sum = 0;
-  while (number <= 21) {
-    prod = number * prod;
-    sum = number + sum;
-    number += 3;
-  }
-  document.getElementById("everyThirdProd").innerHTML = prod;
-  document.getElementById("everyThirdSum").innerHTML = sum;
-}
-
-function compoundFive() {
-  var tbody = document.createElement('tbody'), tr, td, row, cell, amount = 1000;
-  for (row = 1; row <= 10; row++) {
-      amount *= 1.05;
-      tr = document.createElement('tr');
-      for (cell = 0; cell <= 2; cell++) {
-          td = document.createElement('td');
-          if(cell == 0) {
-            td.innerHTML = row;
-          }
-          else if(cell == 1){
-            td.innerHTML = '5%';
-          }
-          else{
-            td.innerHTML = amount.toLocaleString("en", {style: "currency", currency: "USD"});
-          }
-          tr.appendChild(td);
-      }
-      tbody.appendChild(tr);
-  }
-  document.getElementById('compound').appendChild(tbody);
-}
-
-function compoundSix() {
-  var tbody = document.createElement('tbody'), tr, td, row, cell, amount = 1000;
-  for (row = 1; row <= 10; row++) {
-      amount *= 1.06;
-      tr = document.createElement('tr');
-      for (cell = 0; cell <= 2; cell++) {
-          td = document.createElement('td');
-          if(cell == 0) {
-            td.innerHTML = row;
-          }
-          else if(cell == 1){
-            td.innerHTML = '6%';
-          }
-          else{
-            td.innerHTML = amount.toLocaleString("en", {style: "currency", currency: "USD"});
-          }
-          tr.appendChild(td);
-      }
-      tbody.appendChild(tr);
-  }
-  document.getElementById('compound').appendChild(tbody);
-}
-
-function compoundSeven() {
-  var tbody = document.createElement('tbody'), tr, td, row, cell, amount = 1000;
-  for (row = 1; row <= 10; row++) {
-      amount *= 1.07;
-      tr = document.createElement('tr');
-      for (cell = 0; cell <= 2; cell++) {
-          td = document.createElement('td');
-          if(cell == 0) {
-            td.innerHTML = row;
-          }
-          else if(cell == 1){
-            td.innerHTML = '7%';
-          }
-          else{
-            td.innerHTML = amount.toLocaleString("en", {style: "currency", currency: "USD"});
-          }
-          tr.appendChild(td);
-      }
-      tbody.appendChild(tr);
-  }
-  document.getElementById('compound').appendChild(tbody);
-}
-
-// call part 1 functions on load
-function start() {
-  everyFourth();
-  everyThird();
-}
-
-
-// call part 2 functions on load
-function start2() {
-  compoundFive();
-  compoundSix();
-  compoundSeven();
-}
-
-
-$(document).ready(function(){
-
-  $( "#draggable" ).draggable();
-
-});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
