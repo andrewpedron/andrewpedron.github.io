ab8e5bb1dca5988281f9edc56c76f04169e47d42
Andrew Pedron <andrew.pedron@flexport.com>
2020-04-25 11:43:12 -0700

    js

--- cnit133/hw6/hw6.js
+++ cnit133/hw6/hw6.js
@@ -1,44 +1,19 @@
-function validate_checkboxes() {
-
-  if ($('input[type=checkbox]:checked').length == 0) {
-    // alert ( "ERROR! You need to select a checkbox" );
-    document.getElementById("box1").setAttribute('required');
-    document.getElementById("box2").setAttribute('required');
-  } else {
-    document.getElementById("box1").removeAttribute('required');
-    document.getElementById("box2").removeAttribute('required');
-  }
-}
-
-function goToNewPage(myForm) {
-  var mydest = myForm.destList.options[myForm.destList.selectedIndex].value;
-  window.location = mydest;
-}
-
 function process() {
-  var stateData = [
-    ['al', 'alabama', 'Montgomery', '4,887,871'],
-    ['ak', 'alaska', 'Juneau', '737,438'],
-    ['az', 'arizona', 'Phoenix', '7,171,646'],
-    ['ar', 'arkansas', 'Little Rock', '3,010,825'],
-    ['ca', 'california', 'Sacramento', '39,557,045'],
-    ['co', 'colorado', 'denver', '5,694,564'],
-  ];
-  var i;
+  var num;
+  num = document.forms["myform"].elements["userData1"].value;
+  dec = (num.split('.')[1] || []).length;
+  round = Math.round(num);
+  sqrt = Math.round(Math.sqrt(num));
+  tenth = (Math.round(num * 10) / 10).toFixed(1)
+  hundredth = (Math.round(num * 100) / 100).toFixed(2)
+  thousandth = (Math.round(num * 1000) / 1000).toFixed(3)
 
-  userDataRaw = document.forms["myform"].elements["userData1"].value;
-  userData = userDataRaw.toString().toLowerCase();
-  userDataLength = userData.length;
-
-  for (i = 0; i < stateData.length; i++) {
-    if((userData == stateData[i][0]) || (userData == stateData[i][1])) {
-      document.forms["myform"].elements["result"].value = "Here is the information you requested:\n" + "State Abbreviation: " + stateData[i][0].toUpperCase() + "\n"+ "State Name: " + stateData[i][1].charAt(0).toUpperCase() + stateData[i][1].slice(1) + "\n"+ "Capital: " + stateData[i][2] + "\n"+ "Population: " + stateData[i][3] + "\n";
-      break;
-    }
-    else {
-      document.forms["myform"].elements["result"].value = "Your input did not match any of our state records. Please submit a valid state name or state abbreviation.";
-    }
-  }
+  if(dec >= 4) {
+        document.forms["myform"].elements["result"].value = num + " is the number you inputted.\n" + round + " is the nearest integer.\n" + sqrt + " is the square root rounded to the nearest integer.\n" + tenth + " is the number rounded to the nearest tenth.\n" + hundredth + " is the number rounded to the nearest hundredth.\n" + thousandth + " is the number rounded to the nearest thousandth.\n";
+      }
+      else {
+        document.forms["myform"].elements["result"].value = "Your input is not a floating-point number with at least four decimals.";
+      }
 }
 
 
