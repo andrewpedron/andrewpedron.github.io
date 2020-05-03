5aac5b34c8786c356f092f64f02ada2240cde72b
Andrew Pedron <andrew.pedron@flexport.com>
2020-05-03 11:43:16 -0700

    hw7 js

--- cnit133/hw7/hw7.js
+++ cnit133/hw7/hw7.js
@@ -1,3 +1,13 @@
 function changeBodyBg(color){
     document.body.style.background = color;
 }
+
+function changeFontSz(size){
+    document.body.style.fontSize = size;
+}
+
+
+function changeFontFamily() {
+  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  document.body.style.fontFamily = selectedValue;
+}
