6093b2657467974863e96b0a1004d77ea2a98483
Andrew Pedron <andrew.pedron@flexport.com>
2020-05-03 12:08:52 -0700

    part 2

--- cnit133/hw7/hw7.js
+++ cnit133/hw7/hw7.js
@@ -11,3 +11,23 @@ function changeFontFamily() {
   var selectedValue = selectBox.options[selectBox.selectedIndex].value;
   document.body.style.fontFamily = selectedValue;
 }
+
+$(document).ready(function () {
+    $(".hoverli").hover(
+  function () {
+     $('ul.file_menu').slideDown('medium');
+  },
+  function () {
+     $('ul.file_menu').slideUp('medium');
+  }
+);
+
+     $(".file_menu li").hover(
+  function () {
+     $(this).children("ul").slideDown('medium');
+  },
+  function () {
+    $(this).children("ul").slideUp('medium');
+  }
+);
+});
